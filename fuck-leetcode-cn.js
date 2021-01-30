4935bd467f065ea683dbc3727ca16f053595dfc3
Shuxiao WANG <keaising@gmail.com>
2021-01-30 16:52:07 +0800

    Create fuck-leetcode-cn.js

---
+++ fuck-leetcode-cn.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Fuck leetcode-cn.com
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       keaising
+// @match        https://leetcode.com/*
+// @grant        none
+// ==/UserScript==
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function removeDiv() {
+  for (let i = 1; i < 20; i++) {
+    let banner = document.getElementById("cn-banner");
+    if (banner) {
+      banner.remove();
+      break;
+    }
+    await sleep(1000);
+  }
+}
+
+(function () {
+  "use strict";
+  $(document).ready(function () {
+    removeDiv();
+  });
+})();
